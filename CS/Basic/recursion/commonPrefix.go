6236b7742bdacb588257672089b9757720da168a
SHIGEKUNI <zakiru@ezweb.ne.jp>
2024-10-14 22:43:22 +0900

    numberOfDots

--- CS/Basic/recursion/commonPrefix.go
+++ CS/Basic/recursion/commonPrefix.go
@@ -1,15 +1,15 @@
 package main
-import (
-    "fmt"
-)
+// import (
+//     "fmt"
+// )
 func commonPrefix(s1 string, s2 string) string {
-	if  len(s1) == 0 || len(s2) == 0 || string(s1[0]) != string(s2[0]) {
+	if  len(s1) == 0 || len(s2) == 0 || s1[0] != s2[0] {
 		return ""
 	}
 
 	return string(s1[0]) + commonPrefix(s1[1:], s2[1:])
 }
 
-func main(){
-	fmt.Println(commonPrefix("aut","autenim"))
-}
+// func main(){
+// 	fmt.Println(commonPrefix("aut","autenim"))
+// }
---
+++ CS/Basic/recursion/numberOfDots.go
@@ -0,0 +1,16 @@
+package main
+import (
+    "fmt"
+)
+
+func numberOfDots(x int32) int32{
+	if x == 0 {
+		return 0
+	}
+
+    return numberOfDots(x-1) + x
+}
+
+func main(){
+	fmt.Println(numberOfDots(10))
+}
