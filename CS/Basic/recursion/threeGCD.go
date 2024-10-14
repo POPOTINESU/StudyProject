418e6b1282fab4a8a4489c2b39e2dc41fd5cfe72
SHIGEKUNI <zakiru@ezweb.ne.jp>
2024-10-15 07:32:28 +0900

    threeGCD

---
+++ CS/Basic/recursion/threeGCD.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+func threeGCD(x int32, y int32, z int32) int32 {
+	return GCD(GCD(x, y), z)
+}
+
+func GCD(x int32, y int32) int32 {
+	if y == 0 {
+		return x
+	}
+
+	return GCD(y, x%y)
+}
+
+func main() {
+	fmt.Println(threeGCD(12, 18, 24))
+}
