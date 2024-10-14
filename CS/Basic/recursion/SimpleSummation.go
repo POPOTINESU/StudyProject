a1668afda47a9bbdbf102b30527bcfe3051db358
SHIGEKUNI <zakiru@ezweb.ne.jp>
2024-10-14 19:46:33 +0900

    SimpleSummation.go

---
+++ CS/Basic/recursion/SimpleSummation.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+func simpleSummation(n int32) int32 {
+	if n == 0 {
+		return 0
+	}
+	return simpleSummation(n-1) + n
+}
+
+func main() {
+	var n int32
+	fmt.Scan(&n)
+	fmt.Println(simpleSummation(n))
+}
