6e4adca81c7bcc8a55d9088fe5f932245667721e
SHIGEKUNI <zakiru@ezweb.ne.jp>
2024-10-14 21:48:28 +0900

    swapPosition

---
+++ CS/Basic/recursion/SwapPosition.go
@@ -0,0 +1,22 @@
+package main
+
+// import(
+// 	"fmt"
+// )
+
+func swapPosition(s string) string {
+	if len(s) <= 1 {
+		return s
+	}
+
+	if len(s) == 2 {
+		return string(s[1]) + string(s[0])
+	}
+
+	return string(s[1]) + string(s[0]) + swapPosition(s[2:])
+}
+
+
+// func main() {
+// 	fmt.Println(swapPosition("badce"))
+// }
