d5f76e0acda6038689d7109588590dfbac1d2d4c
SHIGEKUNI <zakiru@ezweb.ne.jp>
2024-10-17 00:01:41 +0900

    recursiveIsPalindrome

---
+++ CS/Basic/recursion/recursiveIsPalindrome.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func recursiveIsPalindrome(s string) bool {
+
+	newString := strings.ToLower(strings.ReplaceAll(s, " ", ""))
+
+	rs := stringReverse(newString)
+
+	return isRecursive(newString, rs)
+}
+
+func isRecursive(s string, rs string) bool {
+	if len(s) == 0 {
+		return true
+	}
+
+	if s[0] != rs[0] {
+		return false
+	}
+
+	return isRecursive(s[1:], rs[1:])
+}
+
+func stringReverse(s string) string {
+	rs := []rune(s)
+	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+		rs[i], rs[j] = rs[j], rs[i]
+	}
+
+	return string(rs)
+}
+
+
+func main(){
+	fmt.Println(recursiveIsPalindrome("Top spot"))
+}
