e54fc42c5374599aedd52e6d43a12d6c27ead0bb
SHIGEKUNI <zakiru@ezweb.ne.jp>
2024-10-17 23:33:02 +0900

    doYouFail

---
+++ CS/Basic/controlFlow/doYouFail.go
@@ -0,0 +1,24 @@
+package main
+
+// import (
+// 	"fmt"
+// )
+
+func doYouFail(s string) string {
+	absenceCount := 0
+
+	for i := 0; i < len(s); i += 1 {
+		if string(s[i]) == "A"{
+			absenceCount += 1
+			if absenceCount == 3{
+				return "fail"
+			}
+		}
+	}
+
+	return "pass"
+}
+
+// func main() {
+// 	fmt.Println(doYouFail("AAPPAP"))
+// }
