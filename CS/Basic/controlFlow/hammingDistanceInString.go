f53f2c56899b50abcdd4200aecd7e33c6170065a
SHIGEKUNI <zakiru@ezweb.ne.jp>
2024-10-17 20:57:01 +0900

    hammingDistanceInString

---
+++ CS/Basic/controlFlow/hammingDistanceInString.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+)
+
+func hammingDistanceInString(string1 string, string2 string) int32 {
+	// 文字の数が同じである前提
+	if len(string1) != len(string2) {
+		return 0
+	}
+	if len(string1) == 0{
+		return 0
+	}
+
+	var count int32
+
+	for i := 0; i < len(string1); i++ {
+		if string1[i] != string2[i] {
+			count += 1
+		}
+	}
+
+	return count
+}
+
+func main() {
+	fmt.Println(hammingDistanceInString("toned", "roses"))
+}
