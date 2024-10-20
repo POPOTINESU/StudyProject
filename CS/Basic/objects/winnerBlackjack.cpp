607cea5945f7f61010ba873a345459b9b5f7a92f
SHIGEKUNI <zakiru@ezweb.ne.jp>
2024-10-21 07:36:56 +0900

    convertCardToNums

--- CS/Basic/objects/winnerBlackjack.cpp
+++ CS/Basic/objects/winnerBlackjack.cpp
@@ -3,24 +3,22 @@
 #include <cmath>
 #include <vector>
 #include <iterator>
+#include <algorithm>
 
 using namespace std;
 
 int convertCardToNums(string cardString)
 {
-    if (cardString.length() == 3)
-        return 10;
-    char cardNums = cardString[1];
-    if (cardNums == 'A')
-        return 1;
-    if (cardNums == 'J')
-        return 11;
-    if (cardNums == 'Q')
-        return 12;
-    if (cardNums == 'K')
-        return 13;
-    if (cardNums >= '2' && cardNums <= '9')
-        return cardNums - '0';
+    string targetNum = cardString.substr(3);
+    vector<string> cardNums = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
+
+    int cardNumber;
+
+    for (int i = 0; i < cardNums.size(); i++)
+    {
+        if (targetNum == cardNums[i])
+            return i + 1;
+    }
 
     return 0;
 }
@@ -41,14 +39,10 @@ bool winnerBlackjack(vector<string> playerCards, vector<string> houseCards)
     int houseCardTotal = calculateTotalCards(houseCards);
 
     if (playerCardTotal > 21)
-    {
         return false;
-    }
 
-    if (houseCardTotal < 22 && houseCardTotal > playerCardTotal)
-    {
+    if (houseCardTotal < 22 && houseCardTotal >= playerCardTotal)
         return false;
-    }
 
     return true;
 }
