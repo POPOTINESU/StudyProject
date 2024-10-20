5c0bc789db9418c89335d7bd74c195ceb90332a4
SHIGEKUNI <zakiru@ezweb.ne.jp>
2024-10-20 23:26:03 +0900

    charInBagOfWordsCount

---
+++ CS/Basic/objects/charInBagOfWordsCount.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <vector>
+#include <iterator>
+
+using namespace std;
+
+int charInBagOfWordsCount(vector<string> bagOfWords, char keyCharacter)
+{
+    int charCount = 0;
+
+    for (int i = 0; i < bagOfWords.size(); i++){
+        for (int c = 0; c < bagOfWords[i].length(); c++){
+            if (bagOfWords[i][c] == keyCharacter){
+                charCount ++;
+            }
+        }
+    }
+
+    return charCount;
+}
