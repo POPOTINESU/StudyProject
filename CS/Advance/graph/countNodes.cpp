729b705c8fe33149207b2a3b541506a42ab5cc5e
SHIGEKUNI <zakiru@ezweb.ne.jp>
2024-11-06 23:07:58 +0900

    countNodes

---
+++ CS/Advance/graph/countNodes.cpp
@@ -0,0 +1,27 @@
+#include <cmath>
+#include <deque>
+#include <iostream>
+#include <iterator>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class BinaryTree {
+   public:
+    int data;
+    BinaryTree* left;
+    BinaryTree* right;
+    BinaryTree(int value) { this->data = value; };
+    BinaryTree(int value, BinaryTree* left, BinaryTree* right) {
+        this->data = value;
+        this->left = left;
+        this->right = right;
+    };
+};
+
+int countNodes(BinaryTree* root) {
+    if (root == nullptr) return -1;
+
+    return 1 + countNodes(root->left) + countNodes(root->right);
+}
