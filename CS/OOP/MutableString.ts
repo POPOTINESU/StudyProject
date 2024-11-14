e4365c16587f7ba59d4dd17a0a636bc8ffd084f5
SHIGEKUNI <zakiru@ezweb.ne.jp>
2024-11-15 00:17:50 +0900

    MutableString

---
+++ CS/OOP/MutableString.ts
@@ -0,0 +1,32 @@
+class MutableString {
+  str: Array<string>;
+
+  constructor(string: string) {
+    this.str = [string];
+  }
+
+  append(addString: string) {
+    this.str.push(addString);
+  }
+
+  substring(start: number) {
+    if (start >= this.str.length) {
+      throw new Error("Index out of range");
+    }
+    return this.str.slice(start, this.str.length).join("");
+  }
+
+  concat(strArr: Array<string> | MutableString) {
+    if (strArr instanceof MutableString) {
+      strArr = strArr.str;
+    }
+    strArr.forEach((s: string) => this.str.push(s));
+    return this.str.join("");
+  }
+}
+
+const n: MutableString = new MutableString("aaa");
+console.log(n.substring(1));
+
+const m: MutableString = new MutableString("bbb");
+console.log(n.concat(m));
