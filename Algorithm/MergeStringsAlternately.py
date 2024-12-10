d5d906f4439ceba4e45f21e56c04873bef89bcb9
SHIGEKUNI <zakiru@ezweb.ne.jp>
2024-12-10 19:44:53 +0900

    mergeAlternately

--- Algorithm/MergeStringsAlternately.py
+++ Algorithm/MergeStringsAlternately.py
@@ -1,9 +1,4 @@
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
-        merged_string = "".join(a + b for a, b in zip(word1, word2))
-
-        return merged_string + word1[len(word2) :] + word2[len(word1) :]
-
-
-s = Solution()
-print(s.mergeAlternately(word1="abc", word2="pqr"))
+        merged_string = "".join([c1 + c2 for c1, c2 in zip(word1, word2)])
+        return merged_string + word1[len(word2):] + word2[len(word1)]
