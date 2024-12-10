46befe1915e3b03be8dec8e03fdf4386ce0ab6d4
SHIGEKUNI <zakiru@ezweb.ne.jp>
2024-12-10 19:45:11 +0900

    LRUCache

--- Algorithm/LRUCache.py
+++ Algorithm/LRUCache.py
@@ -5,17 +5,18 @@ class LRUCache:
 
     def __init__(self, capacity: int):
         self.capacity = capacity
-        self.order_dict = OrderedDict()
+        self.cache = OrderedDict()
 
     def get(self, key: int) -> int:
-        if key not in self.order_dict:
-            return -1
-        self.order_dict.move_to_end(key)
-        return self.order_dict[key]
+        if key in self.cache:
+            self.cache.move_to_end(key)
+            return self.cache[key]
+
+        return -1
 
     def put(self, key: int, value: int) -> None:
-        if key in self.order_dict:
-            self.order_dict.move_to_end(key)
-        self.order_dict[key] = value
-        if len(self.order_dict) > self.capacity:
-            self.order_dict.popitem(last=False)
+        if key in self.cache:
+            self.cache.move_to_end(key)
+        self.cache[key] = value
+        if len(self.cache) > self.capacity:
+            self.cache.popitem(last=False)
