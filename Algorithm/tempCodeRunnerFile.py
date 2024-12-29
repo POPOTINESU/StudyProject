2ee08b924e004b1a02f4d2c5d58271c2af93240d
SHIGEKUNI <zakiru@ezweb.ne.jp>
2024-12-29 10:35:29 +0900

    canJump

--- Algorithm/tempCodeRunnerFile.py
+++ Algorithm/tempCodeRunnerFile.py
@@ -1,14 +1,2 @@
-       """
-        Do not return anything, modify matrix in-place instead.
-        """
-        next_matrix = matrix.copy()
-
-        for col in range(len(matrix)):
-            for row in range(len(matrix[0])):
-                target = matrix[col][row]
-                next_matrix[row][len(matrix) - col - 1] = target
-        
-        matrix = next_matrix
-
-
-s = Solution()
+            # if index > max_reachable:
+            #     return False
