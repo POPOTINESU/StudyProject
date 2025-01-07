d289d12f9f9b109d7b9df449266009804f73ae34
SHIGEKUNI <zakiru@ezweb.ne.jp>
2025-01-07 21:54:07 +0900

    myPow

---
+++ Algorithm/myPow.py
@@ -0,0 +1,16 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        if n == 0:
+            return 1
+
+        half = self.myPow(x, abs(n) // 2)
+        if n % 2 == 0:
+            result = half * half
+        else:
+            result = half * half * x
+
+        return result if n > 0 else 1 / result
+
+
+s = Solution()
+print(s.myPow(2.00, -20))
---
+++ Algorithm/practice.py
@@ -0,0 +1,7 @@
+print("テスト")
+print("テスト")
+
+def sum(x: int, y: int)->int:
+    return x + y
+
+print(sum(1,2))
