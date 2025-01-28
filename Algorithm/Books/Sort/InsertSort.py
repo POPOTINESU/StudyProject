b4171dbeaa68a73adfed4d3d9ac73646d44ab16b
SHIGEKUNI <zakiru@ezweb.ne.jp>
2025-01-28 21:08:52 +0900

    insertSort

--- Algorithm/Books/Sort/InsertSort.py
+++ Algorithm/Books/Sort/InsertSort.py
@@ -1,10 +1,9 @@
 def insertSort(total):
     items = list(map(int, input().split(" ")))
 
-    for index in range(1, len(items)):
-        left = index - 1
+    for index in range(1, total):
         target = items[index]
-
+        left = index - 1
         while left >= 0 and items[left] > target:
             items[left + 1] = items[left]
             left -= 1
