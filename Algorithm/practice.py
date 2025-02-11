9c5bb8d2f65bc0ecdc6df5db1793413171b01f1f
SHIGEKUNI <zakiru@ezweb.ne.jp>
2025-02-11 19:46:32 +0900

    insert_sort

--- Algorithm/practice.py
+++ Algorithm/practice.py
@@ -1,7 +1,20 @@
-print("テスト")
-print("テスト")
+def insert_sort(items: list) -> list:
+    total = len(items)
 
-def sum(x: int, y: int)->int:
-    return x + y
+    for current_index in range(1, total):
+        target = items[current_index]
+        sorted_index = current_index - 1
 
-print(sum(1,2))
+        while sorted_index >= 0 and items[sorted_index] > target:
+            items[sorted_index + 1] = items[sorted_index]
+            sorted_index -= 1
+
+        items[sorted_index + 1] = target
+
+    return items
+
+
+if __name__ == "__main__":
+    total = int(input())
+    items = list(map(int, input().split(" ")))
+    print(insert_sort(items))
